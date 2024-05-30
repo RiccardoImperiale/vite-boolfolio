ebf366bb40faa3ef8360f31ab0c5920f13718934
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-30 16:36:09 +0200

    add simple pagination

--- src/components/AppProjects.vue
+++ src/components/AppProjects.vue
@@ -25,6 +25,12 @@ export default {
                     this.loading = false
                 })
                 .catch(err => console.error(err))
+        },
+        prevPage(url) {
+            this.callApi(url);
+        },
+        nextPage(url) {
+            this.callApi(url);
         }
     },
     mounted() {
@@ -37,21 +43,29 @@ export default {
 <template>
     <div class="container projects_container">
         <div class="projects">
-            <ProjectCard v-for="project in projects.data" :key="project.id" />
+            <ProjectCard v-for="project in projects.data" :key="project.id"
+                :imageSrc="base_api_url + '/storage/' + project.image" />
         </div>
-        <div class="pagination">
-
+        <div class="pagination_container">
+            <div class="pagination">
+                <button @click="prevPage(projects.prev_page_url)">
+                    <img class="arrow_left" width="10" src="/img/arrow-light.png" alt="arrow icon">
+                </button>
+                <button class="page_dot" v-for="page in projects.last_page">
+                    <span>{{ page }}</span>
+                </button>
+                <button @click="nextPage(projects.next_page_url)">
+                    <img class="arrow_right" width="10" src="/img/arrow-light.png" alt="arrow icon">
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-/* PROJECTS -----------------------------> */
-.pagination {
-    margin-top: 1rem;
-    height: 50px;
-    width: 100%;
-    background-color: aliceblue;
+.projects_container {
+    padding-top: 5rem 0;
+
 }
 
 .projects {
@@ -59,5 +73,68 @@ export default {
     display: flex;
     flex-wrap: wrap;
     gap: 1rem;
+}
+
+.pagination_container {
+    display: flex;
+    justify-content: center;
+
+    .pagination {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 1rem;
+        padding-inline: 5px;
+        margin-top: 1rem;
+        height: 60px;
+        border-radius: 60px;
+        background-color: var(--pf-gray-900);
+
+        & button {
+            background-color: var(--pf-accent);
+            color: var(--pf-gray-100);
+            width: 50px;
+            aspect-ratio: 1;
+            border-radius: 40px;
+            border: none;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            &:hover {
+                background-color: var(--pf-gray-300);
+            }
+
+            .arrow_left {
+                rotate: 180deg;
+                transform: translateX(1px);
+            }
+
+        }
+
+        .page_dot {
+            width: 15px;
+            height: 15px;
+            background-color: var(--pf-gray-800);
+            transition: .25s ease;
+
+            & span {
+                color: var(--pf-gray-300);
+                position: relative;
+                top: 17px;
+                font-size: 0.7rem;
+                transition: .5s ease;
+            }
+
+            &:hover {
+                background-color: var(--pf-gray-300);
+            }
+
+            &:hover span {
+                color: var(--pf-gray-900);
+
+            }
+        }
+    }
 }
 </style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,13 +2,15 @@
 
 export default {
     name: 'ProjectCard',
-
+    props: {
+        imageSrc: String
+    }
 }
 </script>
 
 <template>
     <div class="project_card">
-        <img src="" alt="">
+        <img :src="imageSrc" alt="">
     </div>
 </template>
 
@@ -20,19 +22,26 @@ export default {
     aspect-ratio: 1;
     border-radius: .5rem;
     z-index: 1;
+    overflow: hidden;
+
+    & img {
+        width: 100%;
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: top;
+        display: block;
+    }
 }
 
 @media screen and (min-width: 720px) {
     .project_card {
         width: calc(100% / 2 - 1rem + (1rem / 3));
-        background-color: lightgreen;
     }
 }
 
-@media screen and (min-width: 1240px) {
+@media screen and (min-width: 1280px) {
     .project_card {
         width: calc(100% / 3 - 1rem + (1rem / 3));
-        background-color: lightgreen;
     }
 }
 </style>
