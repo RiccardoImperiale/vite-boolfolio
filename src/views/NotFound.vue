03eb5908db3efd1499c1cefc8a5161bd2ab728d1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-03 14:30:20 +0200

    add NotFound component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    name: 'NotFound',
+    components: {
+    }
+}
+</script>
+
+<template>
+    <div class="container not_found">
+        <p>not found</p>
+        <router-link :to="{ name: 'home' }">Go Back Home</router-link>
+    </div>
+</template>
+
+<style scoped>
+.not_found {
+    padding: 5rem 0;
+}
+</style>
